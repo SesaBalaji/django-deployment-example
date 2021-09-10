df2d9cddfdd2d55d0701d27b83bcfaa32395c010
SesaBalaji <86557305+SesaBalaji@users.noreply.github.com>
2021-09-10 15:43:55 +0530

    update_settings.py

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+rw0#$k#vxieg6qn^%+0c74##s4k5+-*(6=omin5%&)koz&q+r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['sesabalajidg.pythonanywhere.com']
 
